4cd34fdebffa51cd2cdb8f06bb3da41251d177a6
isvora <isvoranandrei25@gmail.com>
2020-10-13 13:41:12 +0300

    Added  Credentials file

---
+++ constants/Credentials.py
@@ -0,0 +1,5 @@
+API = {
+    "client_id": "14_CHAR_HERE",  # Personal use script (14 characters).
+    "client_secret": "27_CHAR_HERE",  # Secret key (27 characters).
+    "user_agent": "APP_NAME_HERE",  # App name.
+}

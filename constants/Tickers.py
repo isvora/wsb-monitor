5d9498602e11c654a0732b5c7b2ab7e99f745e41
isvora <isvoranandrei25@gmail.com>
2020-10-13 13:58:40 +0300

    Initial tickers to look through

---
+++ constants/Tickers.py
@@ -0,0 +1,10 @@
+TICKERS = [
+    "TSLA", "MSFT", "AAPL", "AMZN", "GOOG", "GOOGL", "FB", "INTC", "CSCO", "CMCSA", "PEP", "ADBE",
+    "NVDA", "AMD", "NFLX", "NET", "FSLY", "PYPL", "COST", "AMGN", "AVGO", "TXN", "SBUX", "QCOM",
+    "MCD", "NIO", "NKLA", "REGI", "ZM"
+]
+
+COMPANIES = [
+    "Tesla", "Microsoft", "Apple", "Amazon", "Google", "Facebook", "Intel", "Cisco", "Nvidia",
+    "Netflix", "Fastly", "Paypal", "Costco", "Zoom"
+]
